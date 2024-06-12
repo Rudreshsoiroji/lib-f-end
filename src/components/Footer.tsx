2f3e33103898245aed7dc6863993b80d9e93f53c
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-13 01:41:49 +0530

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+  
+        <footer className="border-t p-5 text-center">
+            <span>© 2024 Coder&#39;s Book All rights reserved.</span>
+        </footer>
+    
+  )
+}
+
+export default Footer
