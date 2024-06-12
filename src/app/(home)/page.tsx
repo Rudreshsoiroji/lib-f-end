cb87bc858c7700bad71ea83f180d51d1e331b047
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-13 02:04:30 +0530

    add suspense

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import Banner from "./components/Banner";
 import BookList from "./components/BookList";
+import Looding from "@/components/Looding";
 
 export default function Home() {
   return (
     <>
    <Banner/>
+
+   <Suspense fallback={<Looding/>}>
    <BookList/>
+   </Suspense>
+
+
     </>
   );
 }
