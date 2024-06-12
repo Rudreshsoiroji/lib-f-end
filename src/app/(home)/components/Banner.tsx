60f3d8e39683a4d2e312c36a1f56667e9e0211c4
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-13 00:19:17 +0530

    reorganised and add booklist

--- src/app/(home)/components/Banner.tsx
+++ src/app/(home)/components/Banner.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-import paper from  "../../public/paper-bg.jpg"
-import book from "../../public/book.png"
+import paper from  "../../../../public/paper-bg.jpg"
+import book from ".././../../../public/book.png"
 
 function Banner() {
   return (
